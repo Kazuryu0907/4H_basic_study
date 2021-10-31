4e4c915e693bf43002f6e2db3b0753f2e1c29b5a
Kazuryu0907 <41988990+Kazuryu0907@users.noreply.github.com>
2021-11-01 00:23:58 +0900

    add example in getGoal

--- src/getGoal.py
+++ src/getGoal.py
@@ -50,7 +50,6 @@ class getGoal:
         _,self._bin_img = cv2.threshold(cv2.cvtColor(self._masked,cv2.COLOR_BGR2GRAY),thre[0],thre[1],cv2.THRESH_BINARY)
         contours,hierarchy = cv2.findContours(self._bin_img,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
 
-
         index = [i for i,hi in enumerate(hierarchy[0]) if hi[2] == -1]
         contours = [cnt for i,cnt in enumerate(contours) if i in index]
         sort = [cv2.contourArea(cnt) for cnt in contours]
@@ -65,9 +64,29 @@ class getGoal:
         self._masked = cv2.cvtColor(masked,cv2.COLOR_HSV2BGR)
 
     def colors(self):
-        if self.colors == "white":
+        if self._color == "white":
             self._hsv_min = np.array([0,0,200])
             self._hsv_max = np.array([180,45,255])
-        elif self.colors == "green":
+        elif self._color == "green":
             self._hsv_min = np.array([30,64,0])
             self._hsv_max = np.array([90,255,255])
+
+if __name__ == "__main__":
+    gg = getGoal()
+    img = cv2.imread("before.jpg",cv2.IMREAD_COLOR)
+    mask = cv2.imread("mask.png",cv2.IMREAD_GRAYSCALE)
+    img[mask==0] = [0,0,0]
+    goal = gg.getGoal(img,"white",2,[30,200])
+    approx_contours = []
+    for i, cnt in enumerate(goal):
+        # 輪郭の周囲の長さを計算する。
+        arclen = cv2.arcLength(cnt, True)
+        # 輪郭を近似する。
+        approx_cnt = cv2.approxPolyDP(cnt, epsilon=0.01 * arclen, closed=True)
+        approx_contours.append(approx_cnt)
+        # 元の輪郭及び近似した輪郭の点の数を表示する。
+        print(f"contour {i}: before: {len(cnt)}, after: {len(approx_cnt)}")
+    cv2.drawContours(img,approx_contours,-1,(0,0,255),3)
+    cv2.imshow("a",img)
+    cv2.waitKey(0)
+    
